e65bc9b3c9f6efe6679416a53ef83bce37e44057
hieu <hieuvnautotech@gmail.com>
2022-12-12 16:18:47 +0700

    cty

--- tl/src/components/CardHieu/CardHieu.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function CardHieu() {
-  return (
-    <div>CardHieu</div>
-  )
-}
--- tl/src/views/examples/Admin3Page.js
+++ tl/src/views/examples/Admin3Page.js
@@ -1,37 +1,30 @@
-import React from 'react'
+import React from "react";
 import { Button, Table, UncontrolledTooltip } from "reactstrap";
 import { Pagination, PaginationItem, PaginationLink } from "reactstrap";
-import Navhieu from "../../components/Navbars/NavHieu"
-import TableHieu from "../../components/TableHieu/index"
-import {getNews} from "../../components/apis/index"
+import Navhieu from "../../components/Navbars/NavHieu";
+import TableHieu from "../../components/TableHieu/index";
+import { getNews } from "../../components/apis/index";
 import { useEffect, useState } from "react";
-import ModalHieu from "../../components/ModalHieu/index"
-import CardHieu from "../../components/CardHieu/CardHieu.js"
-
+import ModalHieu from "../../components/ModalHieu/index";
+import CardListHieu from "../../components/CardHieu/CardListHieu";
 
 export default function AdminPage() {
-
   const [news, setNews] = useState([]);
 
-  useEffect(() => {   
-    getNews().then((res)=>{
+  useEffect(() => {
+    getNews().then((res) => {
       // const {data} = res
-      console.log("layNews",res)
-      setNews(res.data)
-      
-    }) 
+      console.log("layNews", res);
+      setNews(res.data);
+    });
   }, []);
-
 
   return (
     <>
       <Navhieu></Navhieu>
-      
-      <ModalHieu>
-        
-      </ModalHieu>
-      <CardHieu/>
-   
+
+      <ModalHieu></ModalHieu>
+      <CardListHieu />
     </>
   );
 }
